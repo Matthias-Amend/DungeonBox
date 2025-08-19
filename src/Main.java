ca21501d12f33743b9c18092e99e13a9286400ce
Matthias Amend <matthias-amend@web.de>
2025-08-20 01:01:44 +0200

    Restarted Project

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,3 @@
-import org.jline.reader.Highlighter;
-import org.jline.reader.LineReader;
-import org.jline.reader.LineReaderBuilder;
-import org.jline.terminal.Terminal;
-import org.jline.terminal.TerminalBuilder;
-
-import java.io.IOException;
-
 public class Main {
-
-    private static boolean running = true;
-
-    public static void main(String[] args) throws IOException {
-        Terminal terminal = TerminalBuilder.builder().system(true).build();
-        Highlighter highlighter = HighlighterBuilder.build();
-        LineReader reader = LineReaderBuilder.builder().terminal(terminal).highlighter(highlighter).build();
-        while(running) {
-            String input = reader.readLine("> ");
-            if(input.matches(CommandType.EXIT.getCommandID())) {
-                running = false;
-                break;
-            } else {
-                Command command = new Command(input);
-                String[] arguments = command.getArguments();
-                String argumentString = String.join(", ", arguments);
-                terminal.writer().println(argumentString);
-                terminal.writer().println("Arguments: " + arguments.length);
-            }
-        }
-        terminal.close();
-    }
 
 }
